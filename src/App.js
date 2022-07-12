a970013537edb81aea570cd6b6e23ba123b41232
General Kelvin <asantekelvin133@gmail.com>
2022-07-12 11:32:08 +0000

    horizon

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HorizonWebpage from "./components/HorizonWebpage";
+
+function App() {
+  return (
+    <div>
+      <HorizonWebpage />
+    </div>
+  );
+}
+
+export default App;
